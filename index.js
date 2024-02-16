6e5bfad018b271f975a452e4aadb41f9964ad59c
Graham <blundell.gp@gmail.com>
2024-02-16 15:48:48 -0800

    added timer and countdown, player win, tie or lose messages

--- index.js
+++ index.js
@@ -128,6 +128,34 @@ function rectangularCollision({ rectangle1, rectangle2 }) {
     )
 }
 
+function determineWinner({ player, enemy, timerId}) {
+    clearTimeout(timerId)
+    document.querySelector('#displayText').style.display = 'flex'
+    if (player.health === enemy.health) {
+        document.querySelector('#displayText').innerHTML = 'Tie'
+    } else if (player.health > enemy.health) {
+        document.querySelector('#displayText').innerHTML = 'Player 1 Wins!'
+    } else if (enemy.health > player.health) {
+        document.querySelector('#displayText').innerHTML = 'Player 2 Wins!'
+    }
+}
+
+let timer = 60
+let timerId
+function decreaseTimer() {
+    if (timer > 0) {
+        timerId = setTimeout(decreaseTimer, 1000)
+        timer--
+        document.querySelector('#timer').innerHTML = timer
+    }
+
+    if (timer === 0) {
+        determineWinner({player, enemy, timerId})
+    }
+}
+
+decreaseTimer(timer)
+
 function animate() {
     window.requestAnimationFrame(animate)
     c.fillStyle = 'black'
@@ -176,6 +204,11 @@ function animate() {
         player.health -= 20
         document.querySelector('#playerHealth').style.width = player.health + '%'
     }
+
+        // End game 0 health
+        if (enemy.health <= 0 || player.health <= 0) {
+            determineWinner({ player, enemy, timerId })
+        }
 }
 
 animate()
